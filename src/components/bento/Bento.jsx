11c263c5490aeafaab1e5c9b068f05ef41e7e615
The Briant Carrillo <brianttd24.7@gmail.com>
2025-03-13 18:48:01 -0400

    agregamos ultimas transiciones

--- src/components/bento/Bento.jsx
+++ src/components/bento/Bento.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { BentoItem } from './BentoItem'
 
 const Bento = () => {
-  const bigBentoPart = 'col-span-6 h-90 w-full rounded-md';
-  const smallBentoPart = 'col-span-4 h-90 w-full rounded-md';
+  const bigBentoPart = 'col-span-6 h-90 w-full rounded-2xl';
+  const smallBentoPart = 'col-span-4 h-90 w-full rounded-2xl';
   return (
     <div className="grid grid-flow-row-dense grid-cols-10 grid-rows-2 gap-4">
-      <BentoItem estilo={bigBentoPart} />
-      <BentoItem estilo={smallBentoPart} />
-      <BentoItem estilo={smallBentoPart} />
-      <BentoItem estilo={bigBentoPart} />
+      <BentoItem estilo={bigBentoPart} title={'Lineas de comunicacion'} image={'/lineas.jpg'}/>
+      <BentoItem estilo={smallBentoPart}  title={'Medios de Conexion'} image={'/medios.jpg'}/>
+      <BentoItem estilo={smallBentoPart} title={'Cables y conectores'} image={'/cableado.jpg'}/>
+      <BentoItem estilo={bigBentoPart} title={'Velocidades transmision'} image={'/velocidades.jpg'}/>
     </div>
   )
 }
--- src/components/bento/BentoItem.jsx
+++ src/components/bento/BentoItem.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
-export const BentoItem = ({ estilo }) => {
+import Image from 'next/image'
+export const BentoItem = ({ estilo, title, image }) => {
   return (
-    <div className={`${estilo} bg-amber-200`}>
-      BentoItem
+    <div className={`${estilo} relative overflow-hidden`}>
+      <div className='absolute z-20 bottom-2 left-4 text-white'>
+        <h2>{title}</h2>
+      </div>
+      <div className={`${estilo} relative transition delay-150 duration-1000 ease-in-out hover:scale-110 `}>
+
+      <div className='w-fit h-fit'>
+       <div className="absolute z-10 bottom-0 top-0 w-full h-full bg-gradient-to-b from-transparent from-10% via-black/30 to-black/90 rounded-2xl"></div>
+        <Image 
+          src={image}
+          // width={100}
+          // height={100}
+          fill
+          // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          alt="Any picture of network"
+          className='rounded-2xl transition delay-150 duration-1000 ease-in-out hover:scale-110'
+        />
+      </div>
+
     </div>
+    </div>
+
   )
 }
