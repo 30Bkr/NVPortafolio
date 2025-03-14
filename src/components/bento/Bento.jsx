f9e4e7e79d232bd49a3a304e49435e5af855b20c
The Briant Carrillo <brianttd24.7@gmail.com>
2025-03-14 00:44:27 -0400

    agregamos cambios

--- src/components/bento/Bento.jsx
+++ src/components/bento/Bento.jsx
@@ -15,15 +15,21 @@ const Bento = () => {
         </div>
         <div className={`grid grid-flow-row-dense grid-cols-10 grid-rows-2 gap-4`}>
           <BentoItem estilo={bigBentoPart} title={'¡Descubre el Poder de las Líneas de Comunicación en Redes!'} image={'/lineas.jpg'}
-            content={'Imagina un mundo donde la información fluye sin barreras, donde cada dispositivo, desde tu teléfono hasta tu computadora, está interconectado, compartiendo datos en un abrir y cerrar de ojos. Las líneas de comunicación en redes son el corazón de esta realidad, los canales invisibles que hacen posible la conectividad y el intercambio de ideas en nuestra era digital.'}
+            content={'Imagina un mundo donde la información fluye sin barreras, donde cada dispositivo, desde tu teléfono hasta tu computadora, está interconectado, compartiendo datos en un abrir y cerrar de ojos. Las líneas de comunicación en redes son el corazón de esta realidad...'}
+            // Imagina un mundo donde la información fluye sin barreras, donde cada dispositivo, desde tu teléfono hasta tu computadora, está interconectado, compartiendo datos en un abrir y cerrar de ojos. Las líneas de comunicación en redes son el corazón de esta realidad, los canales invisibles que hacen posible la conectividad y el intercambio de ideas en nuestra era digital.
           />
           <BentoItem estilo={smallBentoPart}  title={`¡Conéctate con el Futuro! Los Medios de Comunicación en Redes son el Futuro.`} image={'/medios.jpg'}
-            content={'En el mundo digital, los medios de comunicación en redes conectan dispositivos y personas, permitiendo un flujo instantáneo de información. Sin ellos, el intercambio de datos sería caótico. Recuerda que detrás de cada conexión hay un universo que transforma nuestras vidas. ¡Explora y disfruta!'}
+            content={'En el mundo digital, los medios de comunicación en redes conectan dispositivos y personas, permitiendo un flujo instantáneo de información. Sin ellos...'}
+            // En el mundo digital, los medios de comunicación en redes conectan dispositivos y personas, permitiendo un flujo instantáneo de información. Sin ellos, el intercambio de datos sería caótico. Recuerda que detrás de cada conexión hay un universo que transforma nuestras vidas. ¡Explora y disfruta!
           />
           <BentoItem estilo={smallBentoPart} title={'Cables y Conectores: Los Héroes Anónimos de la Danza Digital'} image={'/cableado.jpg'}
-            content={'En un mundo donde la información se mueve a la velocidad de la luz, los cables y conectores son los héroes anónimos que sostienen nuestra danza digital, conectando cada rincón de nuestra vida cotidiana. Imagina un océano de datos, donde cada ola representa un mensaje o una idea; los cables que serpentean por nuestras paredes son esenciales para esta inmensa red de comunicación. Los conectores, como el RJ-45, actúan como la llave maestra de la conectividad, haciendo posible que cada conexión y dispositivo funcione, convirtiendo la magia de la red en parte de nuestra realidad diaria.'}
+            content={'En un mundo donde la información fluye a gran velocidad, los cables y conectores son los héroes anónimos que sostienen nuestra vida digital, conectando cada rincón cotidiano. Imagina un océano...'}
+            // En un mundo donde la información fluye a gran velocidad, los cables y conectores son los héroes anónimos que sostienen nuestra vida digital, conectando cada rincón cotidiano. Imagina un océano de datos, donde los cables son esenciales para la comunicación y los conectores, siendo la llave maestra que permite que hace posible la magia de la red en nuestra realidad diaria.
           />
-          <BentoItem estilo={bigBentoPart} title={'Velocidades transmisión'} image={'/velocidades.jpg'}/>
+          <BentoItem estilo={bigBentoPart} title={'Velocidades de transmisión'} image={'/velocidades.jpg'}
+            content={'¡Imagina un mundo donde la información fluye instantáneamente! Las velocidades de transmisión de datos son clave, ya que determinan la rapidez con la que enviamos y recibimos información. En un entorno digital lleno de streaming y aplicaciones en la nube, una conexión veloz asegura experiencias sin...'}
+            //¡Imagina un mundo donde la información fluye instantáneamente! Las velocidades de transmisión de datos son clave, ya que determinan la rapidez con la que enviamos y recibimos información. En un entorno digital lleno de streaming y aplicaciones en la nube, una conexión veloz asegura experiencias sin interrupciones y permite a las empresas innovar y ser competitivas. En este mundo, ¡la velocidad lo es todo!
+          />
         </div>
       </div>
 
