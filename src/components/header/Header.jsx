a9a189eb7fce40819548d70526e1aa89ac89a4c5
The Briant Carrillo <brianttd24.7@gmail.com>
2025-03-13 22:13:22 -0400

    agregamos

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import styles from './Header.module.css'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className={`grid grid-flow-col grid-rows-3 h-44` }>
+    <div className={`grid grid-flow-col grid-rows-3 h-44 text-slate-800` }>
       <div className={`row-span-2 flex justify-between items-center ${styles.this} ${styles.header2}`}>
-        <div>
-          Imagen 
+        <div className='flex items-center w-68'>
+          <Image 
+            src={'/logo.png'}
+            height={58}
+            width={58}
+            alt='Logo de la Universidad Politecnica Territorial de Caracas "Mariscal Sucre"'
+          />
+          <p className='text-center ml-4'>Universidad Politecnica Territorial de Caracas "Mariscal Sucre"</p>
         </div>
         <div>
           presentaciones de cada uno de nosotros
         </div>
       </div>
       <div className={`flex items-center ${styles.header} `}>
-        Navbar
+        <ul className='flex'>
+          <li className='mx-5'>
+            <button className='cursor-pointer  hover:text-rose-500  font-semibold text-[24px]'>Inicio</button>
+          </li>
+          <li className='mx-5 cursor-pointer'>
+            <button className='cursor-pointer  hover:text-rose-500  font-semibold text-[24px]'>Redes</button>
+          </li>
+          <li className='mx-5 cursor-pointer'>
+            <button className='cursor-pointer  hover:text-rose-500  font-semibold text-[24px]'>Comunicaciones</button>
+          </li>
+          <li className='mx-5 cursor-pointer'>
+            <button className='cursor-pointer  hover:text-rose-500  font-semibold text-[24px]'>Infraestructura</button>   
+          </li>
+        </ul>
       </div>
     </div>
   )
