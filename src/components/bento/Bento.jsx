34a1b8c8d0d212b5aa8d992d42c53a857ba139de
The Briant Carrillo <brianttd24.7@gmail.com>
2025-03-14 00:23:38 -0400

    agregamos informacion en bento

--- src/components/bento/Bento.jsx
+++ src/components/bento/Bento.jsx
@@ -3,8 +3,8 @@ import { BentoItem } from './BentoItem'
 import styles from '../../app/page.module.css'
 
 const Bento = () => {
-  const bigBentoPart = 'col-span-6 h-90 w-full rounded-2xl';
-  const smallBentoPart = 'col-span-4 h-90 w-full rounded-2xl';
+  const bigBentoPart = 'col-span-6 h-90 w-full rounded-2xl text-justify';
+  const smallBentoPart = 'col-span-4 h-90 w-full rounded-2xl text-justify';
   return (
     <div className={`w-full`}>
       <div className={`${styles.contenedor} py-8 px-4`}>
@@ -14,9 +14,15 @@ const Bento = () => {
           <p className='text-gray-600'>Los dispositivos se conectan a internet mediante cables, ondas de radio, o a través de redes inalámbricas</p>
         </div>
         <div className={`grid grid-flow-row-dense grid-cols-10 grid-rows-2 gap-4`}>
-          <BentoItem estilo={bigBentoPart} title={'Líneas de comunicación'} image={'/lineas.jpg'}/>
-          <BentoItem estilo={smallBentoPart}  title={'Medios de Conexión'} image={'/medios.jpg'}/>
-          <BentoItem estilo={smallBentoPart} title={'Cables y conectores'} image={'/cableado.jpg'}/>
+          <BentoItem estilo={bigBentoPart} title={'¡Descubre el Poder de las Líneas de Comunicación en Redes!'} image={'/lineas.jpg'}
+            content={'Imagina un mundo donde la información fluye sin barreras, donde cada dispositivo, desde tu teléfono hasta tu computadora, está interconectado, compartiendo datos en un abrir y cerrar de ojos. Las líneas de comunicación en redes son el corazón de esta realidad, los canales invisibles que hacen posible la conectividad y el intercambio de ideas en nuestra era digital.'}
+          />
+          <BentoItem estilo={smallBentoPart}  title={`¡Conéctate con el Futuro! Los Medios de Comunicación en Redes son el Futuro.`} image={'/medios.jpg'}
+            content={'En el mundo digital, los medios de comunicación en redes conectan dispositivos y personas, permitiendo un flujo instantáneo de información. Sin ellos, el intercambio de datos sería caótico. Recuerda que detrás de cada conexión hay un universo que transforma nuestras vidas. ¡Explora y disfruta!'}
+          />
+          <BentoItem estilo={smallBentoPart} title={'Cables y Conectores: Los Héroes Anónimos de la Danza Digital'} image={'/cableado.jpg'}
+            content={'En un mundo donde la información se mueve a la velocidad de la luz, los cables y conectores son los héroes anónimos que sostienen nuestra danza digital, conectando cada rincón de nuestra vida cotidiana. Imagina un océano de datos, donde cada ola representa un mensaje o una idea; los cables que serpentean por nuestras paredes son esenciales para esta inmensa red de comunicación. Los conectores, como el RJ-45, actúan como la llave maestra de la conectividad, haciendo posible que cada conexión y dispositivo funcione, convirtiendo la magia de la red en parte de nuestra realidad diaria.'}
+          />
           <BentoItem estilo={bigBentoPart} title={'Velocidades transmisión'} image={'/velocidades.jpg'}/>
         </div>
       </div>
--- src/components/bento/BentoItem.jsx
+++ src/components/bento/BentoItem.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
-export const BentoItem = ({ estilo, title, image }) => {
+export const BentoItem = ({ estilo, title, image, content, barra }) => {
   return (
     <div className={`${estilo} relative overflow-hidden`}>
-      <div className='absolute z-20 bottom-2 left-4 text-white'>
-        <h2>{title}</h2>
+      <div className='absolute z-20 bottom-4 px-4 text-white'>
+        <h2 className='font-semibold mb-4'>{title}</h2>
+        <p className='font-light'>{content} <a href="/medios/#lineas">Ver mas.</a></p>
       </div>
       <div className={`${estilo} relative transition delay-150 duration-1000 ease-in-out hover:scale-110 `}>
 
       <div className='w-fit h-fit'>
-       <div className="absolute z-10 bottom-0 top-0 w-full h-full bg-gradient-to-b from-transparent from-10% via-black/30 to-black/90 rounded-2xl"></div>
+       <div className="absolute z-10 bottom-0 top-0 w-full h-full bg-gradient-to-b from-transparent from-10% via-black/60 to-black/80 rounded-2xl"></div>
         <Image 
           src={image}
           // width={100}
