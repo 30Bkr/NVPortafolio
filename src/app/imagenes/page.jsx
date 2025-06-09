63b1821875222c3552f8393603d33c2d4eae5f36
The Briant Carrillo <brianttd24.7@gmail.com>
2025-06-09 16:06:38 -0400

    agregamos

--- src/app/imagenes/page.jsx
+++ src/app/imagenes/page.jsx
@@ -9,7 +9,7 @@ const imagenes = () => {
     <div className={`${styles.contenedor}`}>
       <div className='flex pt-20'>
         <div>
-          <Image src={'/firma.jpeg'}
+          <Image src={'/selfie.jpeg'}
             width={320}
             height={600}
             alt='Primera visita a la institución'
@@ -22,7 +22,7 @@ const imagenes = () => {
       </div>
       <div className="w-full p-[0.5px] bg-gradient-to-r from-transparent via-foreground/90 to-transparent mt-8"/>
 
-      <div className='flex mt-4'>
+      <div className='flex mt-4 sm:flex-col sm:bg-red-400'>
         <div>
           <Image src={'/firma.jpeg'}
             width={320}
@@ -34,7 +34,21 @@ const imagenes = () => {
           <h2>Primer visita</h2>
           <p>En esta visita nos encargamos de levantar toda la información relevante para comenzar a realizar el sistema de inscripción</p>
         </div>
-      </div>  
+      </div> 
+
+            <div className='flex mt-4 sm:flex-col sm:bg-red-400'>
+        <div>
+          <Image src={'/pared.jpeg'}
+            width={320}
+            height={600}
+            alt='Primera visita a la institución'
+          ></Image>
+        </div>
+        <div className='h-120'>
+          <h2>Primer visita</h2>
+          <p>En esta visita nos encargamos de levantar toda la información relevante para comenzar a realizar el sistema de inscripción</p>
+        </div>
+      </div>   
     </div>
     
   )
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -8,27 +8,27 @@ export const Cards = () => {
     <div className={`w-full h-full ${styles.bag}`}>
       <div className={`${styles.contenedor} px-5`}>
         <div className={`${styles.cajon}`}>
-          <h2 className='text-xl text-blue-800 mb-4'>Expande tu conocimiento</h2>
-          <p className='text-3xl text-slate-900 mb-8'>¿Te gustaría saber más sobre la infraestructura y reglas de las redes?</p>
-          <p className='text-gray-600'>En este apartado encontrarás todo sobre conectores, protocoles, normas y tecnologías de redes</p>
+          <h2 className='text-xl text-blue-800 mb-4'>Tecnologías</h2>
+          <p className='text-3xl text-slate-900 mb-8'>¿Cuáles fueron las tecnologías utilizadas para realizar este proyecto?</p>
+          <p className='text-gray-600'>En este apartado encontrarás todo sobre la realización de este proyecto</p>
         </div>
         <div className='lg:grid lg:grid-cols-4 lg:gap-4 flex flex-col items-center gap-4 mt-4'>
 
-            <CardsItems title={'Servicios y protocolos'} image={'/internet.jpg'} path={'/infraestructura/#Servicios'}/>
+            <CardsItems title={'HTML'} image={'/internet.jpg'} path={'/infraestructura/#Servicios'}/>
 
-            <CardsItems title={'Puertos TCP/UDP'} image={'/lineas.jpg'} path={'/infraestructura/#puertos'}/>
+            <CardsItems title={'Bootstrap'} image={'/lineas.jpg'} path={'/infraestructura/#puertos'}/>
 
-            <CardsItems title={'Modelo OSI'} image={'/redes.jpg'} path={'/infraestructura/#OSI'}/>
+            <CardsItems title={'PHP'} image={'/redes.jpg'} path={'/infraestructura/#OSI'}/>
 
-            <CardsItems title={'Modelo TCP'} image={'/redes.jpg'} path={'/infraestructura/#TCP'}/>
+            <CardsItems title={'Javascript'} image={'/redes.jpg'} path={'/infraestructura/#TCP'}/>
 
-            <CardsItems title={'Elementos y normas de cableado'} image={'/cableado.jpg'} path={'/infraestructura/#cableado'}/>
+            <CardsItems title={'MYSQL'} image={'/cableado.jpg'} path={'/infraestructura/#cableado'}/>
 
-            <CardsItems title={'Ancho de banda'} image={'/velocidades.jpg'} path={'/infraestructura/#banda'}/>
+            <CardsItems title={'Apache'} image={'/velocidades.jpg'} path={'/infraestructura/#banda'}/>
 
-            <CardsItems title={'Tecnología de redes'} image={'/mundial.jpg'} path={'/infraestructura/#redes'}/>
+            <CardsItems title={'Github'} image={'/mundial.jpg'} path={'/infraestructura/#redes'}/>
 
-            <CardsItems title={'Ver contenido completo'} image={'/redes.jpg'} path={'/infraestructura/#Servicios'}/>
+            <CardsItems title={'Draw.io'} image={'/redes.jpg'} path={'/infraestructura/#Servicios'}/>
         </div>
       </div>
 
